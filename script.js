2c75870513f2c9fd23a9c222e9697270c58e6f98
L1ttleWizard <andrwyandreev2005@gmail.com>
2022-09-24 00:26:54 +0300

    nothing new

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function refreshIframe1() {
+    var ifr1 = document.querySelector('.cat-1');
+    ifr1.src = ifr1.src;
+}
+function refreshIframe2() {
+    var ifr2 = document.querySelector('.cat-2');
+    ifr2.src = ifr2.src; 
+}
